535d2b3ceb7c14db7692b780d42902dab2b25edc
Harrison Riley <hjr2128@columbia.edu>
2024-01-11 15:20:28 -0600

    Moved 2 templates from templates/reviews to new templates/profile directory. Made profile page also not rely on awkward isactive1 and isactive2 variables for frontend management.

--- cdr/static/reviews.js
+++ cdr/static/reviews.js
@@ -1,15 +1,13 @@
-console.log(hallpageContext.order);
-
 newestBtnLabel = document.getElementById("newestBtnLabel");
 newestBtn = document.getElementById("newestBtn");
 popBtnLabel = document.getElementById("popBtnLabel");
 popBtn = document.getElementById("popBtn");
 
-if (hallpageContext.order=="0") {
+if (context.order=="0") {
 	newestBtnLabel.classList.add("active");
 	newestBtn.checked = true;
 }
-else if (hallpageContext.order=="1") {
+else if (context.order=="1") {
 	popBtnLabel.classList.add("active");
 	popBtn.checked = true;
 }
